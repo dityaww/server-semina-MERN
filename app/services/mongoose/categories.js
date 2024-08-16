d4a7490feaa3cbe2b17dd96903529f993e4e9cf8
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-16 19:52:27 +0700

    services ft categories

---
+++ app/services/mongoose/categories.js
@@ -0,0 +1,74 @@
+const Categories = require('../../api/v1/categories/model')
+const { BadRequestError, NotFoundError } = require('../../errors')
+
+const getAllCategories = async () => {
+    const result = await Categories.find()
+
+    return result
+}
+
+const createCategories = async (req) => {
+    const { name } = req.body
+
+    const check = await Categories.findOne({ name })
+
+    // check properti name jika duplikat balikannya bad req (400)
+    if (check) throw new BadRequestError('Kategori nama duplikat')
+    
+    const result = await Categories.create({ name })
+    return result
+}
+
+const getOneCategories = async (req) => {
+    const { id } = req.params
+
+    const result = await Categories.findOne({ _id: id })
+
+    // if id tidak ada
+    if (!result) throw new NotFoundError(`Tidak ada kategori dengan id : ${id}`)
+
+    return result
+}
+
+const updateCategories = async (req) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const check = await Categories.findOne({ 
+        name, 
+        _id: { $ne: id },
+    })
+
+    // jika name nya sama
+    if (check) throw new BadRequestError('Kategori nama duplikat')
+
+    const result = await Categories.findOneAndUpdate(
+        { _id: id }, 
+        { name }, 
+        { new: true, runValidators: true }
+    )
+
+    // if id tidak ada
+    if (!result) throw new NotFoundError(`Tidak ada kategori dengan id : ${id}`)
+
+    return result
+}
+
+const deleteCategories = async (req) => {
+    const { id } = req.params
+
+    const result = await Categories.findOneAndDelete({ _id: id })
+
+    // if id tidak ada
+    if (!result) throw new NotFoundError(`Tidak ada kategori dengan id : ${id}`)
+
+    return result 
+}
+
+module.exports = {
+    getAllCategories,
+    createCategories,
+    getOneCategories,
+    updateCategories,
+    deleteCategories
+}
