807aa1fe4739c953a6835663d09aa605aa97f5a2
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:51:15 +0700

    add conditional ft talents

--- app/services/mongoose/talents.js
+++ app/services/mongoose/talents.js
@@ -8,7 +8,7 @@ const { NotFoundError, BadRequestError } = require('../../errors');
 const getAllTalents = async (req) => {
   const { keyword } = req.query;
 
-  let condition = {};
+  let condition = { organizer: req.user.organizer };
 
   if (keyword) {
     condition = { ...condition, name: { $regex: keyword, $options: 'i' } };
@@ -31,12 +31,12 @@ const createTalents = async (req) => {
   await checkingImage(image);
 
   // cari talents dengan field name
-  const check = await Talents.findOne({ name });
+  const check = await Talents.findOne({ name, organizer: req.user.organizer });
 
   // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara duplikat
   if (check) throw new BadRequestError('pembicara sudah terdaftar');
 
-  const result = await Talents.create({ name, image, role });
+  const result = await Talents.create({ name, image, role, organizer: req.user.organizer });
 
   return result;
 };
@@ -44,7 +44,7 @@ const createTalents = async (req) => {
 const getOneTalents = async (req) => {
   const { id } = req.params;
 
-  const result = await Talents.findOne({ _id: id })
+  const result = await Talents.findOne({ _id: id, organizer: req.user.organizer })
     .populate({
       path: 'image',
       select: '_id name',
@@ -74,6 +74,7 @@ const updateTalents = async (req) => {
   const check = await Talents.findOne({
     name,
     _id: { $ne: id },
+    organizer: req.user.organizer
   });
 
   // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara nama duplikat
@@ -81,7 +82,7 @@ const updateTalents = async (req) => {
 
   const result = await Talents.findOneAndUpdate(
     { _id: id },
-    { name, image, role },
+    { name, image, role, organizer: req.user.organizer },
     { new: true, runValidators: true }
   );
 
@@ -93,6 +94,7 @@ const deleteTalents = async (req) => {
 
   const result = await Talents.findOneAndDelete({
     _id: id,
+    organizer: req.user.organizer
   });
 
   if (!result)
