ddf808b9ac5836e5cbec46612751c0a98bd78942
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:50:41 +0700

    add conditional in events services

--- app/services/mongoose/events.js
+++ app/services/mongoose/events.js
@@ -8,8 +8,8 @@ const { checkingTalents } = require('./talents');
 const { NotFoundError, BadRequestError } = require('../../errors');
 
 const getAllEvents = async (req) => {
-  const { keyword, category, talent } = req.query;
-  let condition = {};
+  const { keyword, category, talent, status } = req.query;
+  let condition = { organizer: req.user.organizer };
 
   if (keyword) {
     condition = { ...condition, title: { $regex: keyword, $options: 'i' } };
@@ -23,6 +23,10 @@ const getAllEvents = async (req) => {
     condition = { ...condition, talent: talent };
   }
 
+  if (['Draft', 'Published'].includes(status)) {
+    condition = { ...condition, statusEvent: status }
+  }
+
   const result = await Events.find(condition)
     .populate({ path: 'image', select: '_id name' })
     .populate({
@@ -32,7 +36,7 @@ const getAllEvents = async (req) => {
     .populate({
       path: 'talent',
       select: '_id name role image',
-      populate: { path: 'image', select: '_id  name' },
+      populate: { path: 'image', select: '_id name' },
     });
 
   return result;
@@ -76,6 +80,7 @@ const createEvents = async (req) => {
     image,
     category,
     talent,
+    organizer: req.user.organizer
   });
 
   return result;
@@ -84,7 +89,7 @@ const createEvents = async (req) => {
 const getOneEvents = async (req) => {
   const { id } = req.params;
 
-  const result = await Events.findOne({ _id: id })
+  const result = await Events.findOne({ _id: id, organizer: req.user.organizer })
     .populate({ path: 'image', select: '_id name' })
     .populate({
       path: 'category',
@@ -132,6 +137,7 @@ const updateEvents = async (req) => {
   // cari Events dengan field name dan id selain dari yang dikirim dari params
   const check = await Events.findOne({
     title,
+    organizer: req.user.organizer,
     _id: { $ne: id },
   });
 
@@ -152,6 +158,7 @@ const updateEvents = async (req) => {
       image,
       category,
       talent,
+      organizer: req.user.organizer
     },
     { new: true, runValidators: true }
   );
@@ -164,6 +171,7 @@ const deleteEvents = async (req) => {
 
   const result = await Events.findOneAndDelete({
     _id: id,
+    organizer: req.user.organizer
   });
 
   if (!result) throw new NotFoundError(`Tidak ada events dengan id :  ${id}`);
@@ -171,10 +179,34 @@ const deleteEvents = async (req) => {
   return result;
 };
 
+const changeStatusEvents = async (req) => {
+  const { id } = req.params
+  const { statusEvent } = req.body
+
+  if (!['Draft', 'Published'].includes(statusEvent)) {
+    throw new BadRequestError('Status harus draft atau published')
+  }
+
+  const checkEvent = await Events.findOne({
+    _id: id,
+    organizer: req.user.organizer
+  })
+
+  if (!checkEvent) {
+    throw new NotFoundError(`Tidak ada cara dengan id ${id}`)
+  }
+
+  checkEvent.statusEvent = statusEvent
+
+  await checkEvent.save()
+  return checkEvent
+}
+
 module.exports = {
   getAllEvents,
   createEvents,
   getOneEvents,
   updateEvents,
   deleteEvents,
+  changeStatusEvents
 };
