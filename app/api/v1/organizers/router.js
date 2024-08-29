b212d1f2a5a8f9546329cc93567439a4551a9f31
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:48:09 +0700

    update router ft organizer

--- app/api/v1/organizers/router.js
+++ app/api/v1/organizers/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express();
-const { createCMSOrganizer } = require('./controller');
+const { createCMSOrganizer, createCMSUser, getCMSUsers } = require('./controller');
+const { authenticateUser, authorizeRoles } = require('../../../middlewares/auth')
 
-router.post('/organizers', createCMSOrganizer);
+router.post('/organizers', authenticateUser, authorizeRoles('owner'), createCMSOrganizer); // create organizer hanya role owner
+router.post('/users', authenticateUser, authorizeRoles('organizer'), createCMSUser); // create admin hanya role organizer
+router.get('/users', authenticateUser, authorizeRoles('owner'), getCMSUsers); // get all user
 
 module.exports = router;
