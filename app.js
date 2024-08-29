6ba9004dfc139aae147f526553d5c7c3159d149f
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:52:00 +0700

    import route orders

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const talentsRouter = require('./app/api/v1/talents/router')
 const eventsRouter = require('./app/api/v1/events/router')
 const organizerRouter = require('./app/api/v1/organizers/router')
 const authCMSRouter = require('./app/api/v1/auth/router')
+const ordersRouter = require('./app/api/v1/orders/router')
 
 const v1 = '/api/v1/cms'
 
@@ -38,6 +39,7 @@ app.use(v1, talentsRouter)
 app.use(v1, eventsRouter)
 app.use(v1, organizerRouter)
 app.use(v1, authCMSRouter)
+app.use(v1, ordersRouter)
 
 app.use(notFoundMiddleware)
 app.use(handleErrorMiddleware)
