ab3d626b0f3a01523f32fe6092df87bec7ba2a9e
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:45:36 +0700

    update router categories

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express()
-
 const { create, index, find, update, destroy } = require('./controller')
 
-router.get('/categories', index) // get all
-
-router.get('/categories/:id', find) // get one categories
-
-router.put('/categories/:id', update) // update
-router.delete('/categories/:id', destroy) // delete
-router.post('/categories', create) // create
+const { authenticateUser, authorizeRoles } = require('../../../middlewares/auth')
+ 
+router.get('/categories', authenticateUser, authorizeRoles('organizer'), index) // get all
+router.get('/categories/:id', authenticateUser, authorizeRoles('organizer'), find) // get one categories
+router.put('/categories/:id', authenticateUser, authorizeRoles('organizer'), update) // update
+router.delete('/categories/:id', authenticateUser, authorizeRoles('organizer'), destroy) // delete
+router.post('/categories', authenticateUser, authorizeRoles('organizer'), create) // create
 
 module.exports = router
