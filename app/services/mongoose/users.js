3fe55de7944ef24b81814536c9e08d1e73cf296b
dityaww <111202012838@mhs.dinus.ac.id>
2024-08-29 17:51:44 +0700

    update services ft users

--- app/services/mongoose/users.js
+++ app/services/mongoose/users.js
@@ -1,6 +1,7 @@
 const Users = require('../../api/v1/users/model')
 const Organizer = require('../../api/v1/organizers/model')
 const { BadRequestError } = require('../../errors')
+const { StatusCodes } = require('http-status-codes')
 
 const createOrganizer = async ( req ) => {
     const { organizer, name, email, password, confirmPassword, role } = req.body
@@ -24,4 +25,28 @@ const createOrganizer = async ( req ) => {
     return users
 }
 
-module.exports = { createOrganizer }
+const createUser = async (req, res) => {
+    const { name, password, confirmPassword, role, email} = req.body
+
+    if (password !== confirmPassword) {
+        throw new BadRequestError('Password dan konfirmasi password tidak cocok')
+    }
+
+    const result = await Users.create({
+        name,
+        email, 
+        organizer: req.user.organizer,
+        password,
+        role
+    })
+
+    return result
+}
+
+const getAllUsers = async (req) => {
+    const result = await Users.find()
+
+    return result 
+}
+
+module.exports = { createOrganizer, createUser, getAllUsers }
